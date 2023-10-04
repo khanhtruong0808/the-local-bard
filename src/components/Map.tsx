f28903521f552857bb69b1bd7ed941dc7b3d55ff
Jared Sun <jaredsun1@gmail.com>
2023-10-03 18:49:09 -0700

    add image and click functionality to map sidebar productions

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -213,17 +213,30 @@ export default function Map() {
                 <div
                   key={production.id}
                   className="cursor-pointer overflow-hidden rounded-lg bg-zinc-700 shadow hover:bg-zinc-600"
+                  onClick={() => handleActiveProduction(production)}
                 >
-                  <div className="px-4 py-5 sm:p-6">
-                    <h3 className="text-lg font-medium leading-6 text-zinc-200">
-                      {production.name}
-                    </h3>
-                    <div className="mt-2 max-w-xl text-sm text-zinc-400">
-                      <p>{theater?.name}</p>
-                      <p>{address?.street_address}</p>
-                      <p>
-                        {address?.city}, {address?.state}
-                      </p>
+                  <div className="sm:flex">
+                    <div className="ml-2 flex-shrink-0 self-center pl-4 sm:mb-0 sm:mr-4">
+                      {production.poster_url && (
+                        <Image
+                          src={production.poster_url}
+                          alt={production.name || "Production poster"}
+                          width={100}
+                          height={100}
+                        />
+                      )}
+                    </div>
+                    <div className="px-4 py-5 sm:p-6">
+                      <h3 className="text-lg font-medium leading-6 text-zinc-200">
+                        {production.name}
+                      </h3>
+                      <div className="mt-2 max-w-xl text-sm text-zinc-400">
+                        <p>{theater?.name}</p>
+                        <p>{address?.street_address}</p>
+                        <p>
+                          {address?.city}, {address?.state}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
