b15724f35cbf90471c7993fdfc64fb11a088ed62
Khanh Truong <khanhtroung@gmail.com>
2023-12-06 17:40:03 -0800

    fix cancel button deleting a stage/production

--- src/components/ConfirmDeleteForm.tsx
+++ src/components/ConfirmDeleteForm.tsx
@@ -1,3 +1,4 @@
+import useDialog from "@/utils/dialogStore";
 import Button from "./ui/Button";
 import SubmitButton from "./ui/SubmitButton";
 
@@ -6,11 +7,15 @@ interface ConfirmDeleteFormProps {
 }
 
 export const ConfirmDeleteForm = ({ handleDelete }: ConfirmDeleteFormProps) => {
+  const { closeDialog } = useDialog();
+
   return (
     <form action={handleDelete}>
       <p className="py-3 text-sm text-zinc-300">Are you sure?</p>
       <div className="flex justify-end">
-        <Button variant="secondary">Cancel</Button>
+        <Button variant="secondary" type="button" onClick={closeDialog}>
+          Cancel
+        </Button>
         <SubmitButton variant="alert" className="ml-3">
           Delete
         </SubmitButton>
