990a8123f6c81205a81a120f76b4d7a6b53bd5f5
Jared Sun <jaredsun1@gmail.com>
2024-03-06 17:00:20 -0800

    revert navbar change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,6 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
-import { Button } from "@/components/ui/button";
-
 export const Navbar = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const isSearchPage = pathname === "/search";
@@ -30,47 +28,36 @@ export const Navbar = ({ children }: { children: React.ReactNode }) => {
           {/* Desktop Navbar */}
           <div
             className={twMerge(
-              "mx-auto hidden h-20 w-full max-w-5xl p-8 md:block",
+              "mx-auto hidden h-20 w-full max-w-5xl items-center p-8 md:flex",
               isSearchPage && "bg-zinc-900",
             )}
           >
-            <div className="relative flex h-20 justify-between">
-              <div className="relative z-10 flex px-2 lg:px-0">
-                <div className="flex flex-shrink-0 items-center">
-                  <Link
-                    href="/"
-                    className="mr-14 flex items-center gap-2 whitespace-nowrap font-medium"
-                  >
-                    <Image width={40} height={40} alt="" src="/logo.jpg" />
-                    The Local Bard
-                  </Link>
-                </div>
+            <Link
+              href="/"
+              className="mr-14 flex items-center gap-2 whitespace-nowrap font-medium"
+            >
+              <Image width={40} height={40} alt="" src="/logo.jpg" />
+              The Local Bard
+            </Link>
+            <div className="flex w-full justify-between">
+              <div className="flex items-center gap-x-4 text-sm">
+                {links.map(
+                  (link) =>
+                    !link.hidden && (
+                      <Link
+                        key={link.label}
+                        href={link.href}
+                        className={twMerge(
+                          "rounded px-3 py-2 text-zinc-400 hover:bg-white/10 hover:text-white",
+                          isSearchPage && "hover:bg-zinc-700",
+                        )}
+                      >
+                        {link.label}
+                      </Link>
+                    ),
+                )}
               </div>
-              <div className="relative z-0 flex flex-1 items-center justify-center px-2 sm:absolute sm:inset-0">
-                <div className="w-full sm:max-w-xs">
-                  <div className="flex items-center justify-center gap-x-4 text-sm">
-                    {links.map(
-                      (link) =>
-                        !link.hidden && (
-                          <Link
-                            key={link.label}
-                            href={link.href}
-                            className={twMerge(
-                              "rounded px-3 py-2 text-sm font-medium text-zinc-400 hover:bg-white/10 hover:text-white",
-                              isSearchPage && "hover:bg-zinc-700",
-                            )}
-                          >
-                            {link.label}
-                          </Link>
-                        ),
-                    )}
-                  </div>
-                </div>
-              </div>
-              {/* Profile dropdown slot */}
-              <div className="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center">
-                {children}
-              </div>
+              {children}
             </div>
           </div>
 
